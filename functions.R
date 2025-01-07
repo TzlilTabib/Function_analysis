688424570986d835608f27165ab07a0d0003ccc3
Tzlil Tabib <tzliltabib@gmail.com>
2025-01-07 15:09:00 +0200

    4th commit for debugging and improving readability
    Improving readability of the function by adding spaces, headers and correcting indentation. In addition, changing saving method to enable using source() in other files.

--- functions.R
+++ functions.R
@@ -1,34 +1,50 @@
+library(dplyr)
+
 ### FUNCTION TO SUMMARIZE DESCRIPTIVE STATISTICS OF A DATA FRAME --------------- 
-summarize_descriptives <- function(df) {
+summarize_descriptives <- function(df, subject_start, subject_end) {
+  # Checking the data length
   if (nrow(df) < 10) {
-    stop("data is too short")}
+    stop("data is too short")
+  }
+  # Filtering data by specified parameters
+  filtered_data <- df |>
+    filter(subject >= subject_start, subject <= subject_end)
+  
+  ## Creating descriptive statistics table
   descriptives <- data.frame()
-  for (col_name in names(df)) {
-    variable <- df[[col_name]]
-  # Descriptive statistics for continuous variables
-  if (class(variable) %in% c("numeric", "integer")) {
-    descriptives <- rbind(descriptives, data.frame(
-      variable = col_name,
-      type     = "continuous",
-      min      = min(variable, na.rm = TRUE),
-      max      = max(variable, na.rm = TRUE),
-      mean     = mean(variable, na.rm = TRUE),
-      sd       = sd(variable, na.rm = TRUE),
-      levels   = "-",
-      count    = "-"))
-  # Descriptive statistics for categorical variables
-  } else if (class(variable) %in% c("factor", "character")) {
-    levels_count <- data.frame(table(variable, useNA = "ifany"))
-    descriptives <- rbind(descriptives, data.frame(
-      variable = col_name,
-      type     = "categorical",
-      min      = "-",
-      max      = "-",
-      mean     = "-",
-      sd       = "-",
-      levels   = paste(levels_count$variable, collapse = ", "),
-      count    = paste(levels_count$Freq, collapse = ", ")))}}
-  return(descriptives)}
+  
+  for (col_name in names(filtered_data)) {
+    variable <- filtered_data[[col_name]]
+  
+    # Descriptive statistics for continuous variables
+    if (class(variable) %in% c("numeric", "integer")) {
+      descriptives <- rbind(descriptives, data.frame(
+        variable = col_name,
+        type     = "continuous",
+        min      = min(variable, na.rm = TRUE),
+        max      = max(variable, na.rm = TRUE),
+        mean     = mean(variable, na.rm = TRUE),
+        sd       = sd(variable, na.rm = TRUE),
+        levels   = "-",
+        count    = "-"))
+      }
+  
+    # Descriptive statistics for categorical variables
+    else if (class(variable) %in% c("factor", "character")) {
+      levels_count <- data.frame(table(variable, useNA = "ifany"))
+      descriptives <- rbind(descriptives, data.frame(
+        variable = col_name,
+        type     = "categorical",
+        min      = "-",
+        max      = "-",
+        mean     = "-",
+        sd       = "-",
+        levels   = paste(levels_count$variable, collapse = ", "),
+        count    = paste(levels_count$Freq, collapse = ", ")))
+      }
+    }
+  return(descriptives)
+}
 
 ## Saving the file
-save(summarize_descriptives, file = './functions.R')
+writeLines(deparse(summarize_descriptives), './functions.R')
