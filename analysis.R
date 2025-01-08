ee0e736803c24c161e87c00cbd106028ddb3121b
Tzlil Tabib <tzliltabib@gmail.com>
2025-01-08 10:32:46 +0200

    6th commit - importing and using function
    Importing "functions" script using source, and using the function 'summarize_descriptives'.

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
+### GENRERATING THE DATA FRAME -------------------------------------------------
 N = 100
 df <- data.frame(
-  subject_code       = 1:N,
+  subject            = 1:N,
   age                = runif(N, min = 18, max = 35),
   gender             = sample(factor(c("male", "female")), size = N, replace = TRUE),
   avg_rt             = rnorm(N, mean = 1500, sd = 300),
@@ -8,5 +9,9 @@ df <- data.frame(
   avg_sleep_duration = rnorm(N, mean = 6.5, sd = 2)
 )
 
+# Saving the data frame
 save(df, file = './df.rdata')
 
+### SUMMARIZING DESCRIPTIVE STATISTICS USING A CUSTOM FUNCTION -----------------
+source("./functions.R")
+summarize_descriptives(df, 1, N)
